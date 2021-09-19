dcc8b3f756f5899c42f299b3b249eda5aacad71d
t2 <oo_ri@naver.com>
2021-09-19 18:08:25 +0900

    Character Postion Movement Update

---
+++ 01_react/src/api/animation.ts
@@ -0,0 +1,6 @@
+import { keyframes } from '@emotion/react';
+
+export const spriteAnimation = keyframes`
+  from { background-position-x: 0 }
+  to { background-position-x: -3770px }
+`;
--- 01_react/src/components/Character.tsx
+++ 01_react/src/components/Character.tsx
@@ -1,18 +1,23 @@
 import { FC, memo, useEffect, useState } from 'react';
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { Keyboard } from '../api/keyboard';
+import { spriteAnimation } from '../api/animation';
+import { clearInterval, setInterval } from 'timers';
 
-const Container = styled.div({
+const SPEED = 10;
+
+interface IContainer {
+  position: number;
+}
+
+const Container = styled.div<IContainer>(({ position }) => ({
   width: 377,
   height: 458,
-});
-
-const Animation = keyframes`
-  from { background-position-x: 0 }
-  to { background-position-x: -3770px }
-`;
+  position: 'fixed',
+  bottom: 0,
+  left: position,
+}));
 
 type Motion = 'idle' | 'run' | 'attack';
 
@@ -25,7 +30,7 @@ const SpriteImage = styled.div<ISpriteImage>(({ motion, isFlip }) => ({
   height: '100%',
   backgroundImage: `url(${process.env.PUBLIC_URL}/image/character/${motion}.png)`,
   transform: isFlip ? 'rotateY(180deg)' : undefined,
-  animation: `${Animation} 0.5s infinite steps(10)`,
+  animation: `${spriteAnimation} 0.5s infinite steps(10)`,
 }));
 
 interface ICharacter {
@@ -35,27 +40,36 @@ interface ICharacter {
 const Character: FC<ICharacter> = ({ keyboard }) => {
   const [motion, setMotion] = useState<Motion>('idle');
   const [isFlip, setIsFlip] = useState(false);
+  const [position, setPosition] = useState(0);
 
   useEffect(() => {
-    if (keyboard.ArrowLeft !== keyboard.ArrowRight) {
-      if (keyboard.ArrowLeft) {
-        setIsFlip(true);
-      }
+    const interval = setInterval(() => {
+      if (keyboard.ArrowLeft !== keyboard.ArrowRight) {
+        if (keyboard.ArrowLeft) {
+          setIsFlip(true);
+          setPosition((prev) => prev - SPEED);
+        }
 
-      if (keyboard.ArrowRight) {
-        setIsFlip(false);
-      }
+        if (keyboard.ArrowRight) {
+          setIsFlip(false);
+          setPosition((prev) => prev + SPEED);
+        }
 
-      setMotion('run');
-    } else if (keyboard.Space) {
-      setMotion('attack');
-    } else {
-      setMotion('idle');
-    }
+        setMotion('run');
+      } else if (keyboard.Space) {
+        setMotion('attack');
+      } else {
+        setMotion('idle');
+      }
+    }, 20);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [keyboard]);
 
   return (
-    <Container>
+    <Container position={position}>
       <SpriteImage motion={motion} isFlip={isFlip} />
     </Container>
   );
